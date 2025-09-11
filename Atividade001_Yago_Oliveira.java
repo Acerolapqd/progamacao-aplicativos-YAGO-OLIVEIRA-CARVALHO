37d438e5bdf11ada48a30a6046a9978f044b3b67
Yago Oliveira <yagooliveira8009@gmail.com>
2025-09-11 20:44:47 -0300

    Atividade001_yago_oliveira
    11/09/25

---
+++ Atividade001_Yago_Oliveira.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        System.out.println("OLÁ");
+
+
+    int A = 25;
+    int B = A * 3;
+    int C = 10;
+    int D = A / C;
+    System.out.println("A = " + A);
+    System.out.println("B = " + B);
+    System.out.println("C = " + C);
+    System.out.println("D = " + D);
+
+
+    }
+}
