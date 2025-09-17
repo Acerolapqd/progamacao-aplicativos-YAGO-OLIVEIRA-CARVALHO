970072e54c59419393db250247c891eb7c13a2fc
Yago Oliveira <yagooliveira8009@gmail.com>
2025-09-16 21:09:54 -0300

    Add files via upload

---
+++ atividade02_yago_oliveira.java
@@ -0,0 +1,166 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade02_gabriell_avila_lima;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade02_gabriell_avila_lima {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //System.out.println("Olá mundo!");
+        //System.out.print("Olá mundo!");
+        //System.out.println("Olá/Fulano/");
+        
+        //1
+        //Scanner entrada = new Scanner(System.in);
+        //double raio,area,circunf;
+        
+        //System.out.print("Digite o valor do raio do círculo:");
+        //raio = entrada.nextDouble();
+        
+        //area = Math.PI*(Math.pow(raio,2));
+    
+        //circunf = 2*Math.PI*raio;
+        
+        //System.out.println("Resultado área:"+area);
+        //System.out.println("Resultado circunferência:"+circunf);
+        
+        
+        //2
+        //Scanner entrada = new Scanner(System.in);
+        //double peso,altura,imc;
+        
+        //System.out.print("Digite o seu peso:");
+        //peso = entrada.nextDouble();
+        
+        //System.out.print("Digite sua altura:");
+        //altura = entrada.nextDouble();
+        
+        //imc = peso/(Math.pow(altura, 2));
+        //System.out.println("Resultado:"+imc);
+        
+        //3
+        //Scanner entrada = new Scanner(System.in);
+        //double cateto1,cateto2,hipotenusa;
+        
+        //System.out.print("Digite o cateto 1:");
+        //cateto1= entrada.nextDouble();
+        
+        //System.out.print("Digite o cateto 2:");
+        //cateto2= entrada.nextDouble();
+        
+        //hipotenusa = Math.sqrt(Math.pow(cateto1, 2)+ Math.pow(cateto2, 2));
+        //System.out.print("O resultado é: %.3f", hipotenusa);
+        
+        //4
+        //Scanner entrada = new Scanner(System.in);
+        //double r,d,d_c,t_r;
+        
+        //System.out.println("Digite um valor em real:");
+        //r = entrada.nextDouble(); 
+        
+        //d = 5.32;
+        //d_c = Math.floor(r/d);
+        //t_r = r - (d_c * d);
+          
+        //System.out.println("Com essa quantia em reais, você pode comprar:" + d_c);
+        //System.out.println("Troco em reais:"+ t_r);
+        
+        //5
+        //Scanner entrada = new Scanner(System.in);
+        //int n,j1,j2,soma;
+        
+        //n = entrada.nextInt();
+        //j1 = entrada.nextInt();
+        //j2 = entrada.nextInt();
+        //soma = (j1+j2)/n;
+        
+        //System.out.println("A média é:"+soma);
+        //System.out.println(Math.max(j1, j2));
+        //System.out.println(Math.min(j1, j2));
+        
+        //6
+        //Scanner entrada = new Scanner(System.in);
+        //double angulo,tan,sen,cos,rad;
+        
+        //System.out.println("Insira o valor de um ângulo:");
+        //angulo = entrada.nextDouble();
+        
+        //rad = Math.toRadians(angulo);
+        //tan = Math.tan(rad);
+        //sen = Math.sin(rad);
+        //cos = Math.cos(rad);
+        
+        //System.out.println("Tangente desse ângulo:"+tan);
+        //System.out.println("Seno desse ângulo:"+sen);
+        //System.out.println("Cosseno desse ângulo:"+cos);
+        
+        //7
+        //Scanner entrada = new Scanner(System.in);
+        //double x1,x2,y1,y2,calc;
+        
+        //x1 = entrada.nextDouble();
+        //x2 = entrada.nextDouble();
+        //y1 = entrada.nextDouble();
+        //y2 = entrada.nextDouble();
+        
+        //calc = Math.sqrt(Math.pow((x2-x1), 2)) + (Math.pow((y1-y2), 2));
+        
+        //System.out.println("A distância euclediana é:" +calc)
+        
+        //8
+        //Scanner entrada = new Scanner(System.in);
+        //double n1,n2,n3,media,soma,dif;
+        
+        //n1 = entrada.nextDouble();
+        //n2 = entrada.nextDouble();
+        //n3 = entrada.nextDouble();
+        
+        //media = (n1+n2+n3)/3;
+        //soma = Math.abs(n1)+Math.abs(n2)+Math.abs(n3);
+        //dif = Math.abs(n1-n3);
+        
+        //System.out.println("A média é:"+media);
+        //System.out.println("A soma dos valores absolutos é:"+soma);
+        //System.out.println("A maior diferença é:"+dif);
+        
+        //9
+        //Scanner entrada = new Scanner(System.in);
+        //int a,b,c;
+        //double x1,x2,delta;
+        
+        //System.out.println("Insira o valor de a:");
+        //a = entrada.nextInt();
+        
+        //System.out.println("Insira o valor de b:");
+        //b = entrada.nextInt();
+        
+        //System.out.println("Insira o valor de c:");
+        //c = entrada.nextInt();
+        
+        //delta = Math.sqrt((Math.pow(b,2)) - (4*a*c));
+        //x1 = ((0-b)+delta)/(2*a);
+        //x2 = ((0-b)-delta)/(2*a);
+        
+        //System.out.println("O valor de x1 é:"+x1);
+        //System.out.println("O valor de x2 é:"+x2);
+        
+        //10
+        //Scanner entrada = new Scanner(System.in);
+        //double far,cel;
+        
+        //System.out.println("Digite o valor em Fahrenheit F:");
+        //far = entrada.nextDouble();
+        
+        //cel = (far-32)/1.0;
+        //System.out.println("O valor de F "+far+" em Fahrenheit é equivalente a C "+cel);
+    }
+}
