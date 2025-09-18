fc06a423a39279254346a7962bdd6fb9342a77cf
Yago Oliveira <yagooliveira8009@gmail.com>
2025-09-17 21:42:00 -0300

    Add files via upload

---
+++ atividade003_yagooliveira.java
@@ -0,0 +1,243 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade03_gabriel_avila.java;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade03_gabriel_avilaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //1
+        //Scanner entrada = new Scanner(System.in);
+        //int p,a,calc,aN;
+        
+        //p = 8;
+        //a = 200;
+        //calc = a/p;
+        
+        //System.out.print("Digite a quantia de porções:");
+        //p = entrada.nextInt ();
+        
+        //aN = p*calc;
+                
+        //System.out.print("Quantia necessária de açúcar em gramas:"+aN);
+
+        //2
+        //Scanner entrada = new Scanner(System.in);
+        //int n,p,m,calc;
+        
+        //p = 180;
+        //m = 12;
+        
+        //System.out.print("Digite a quantidade de impressoras:");
+        //n = entrada.nextInt();
+        
+        //calc = p/n;
+        
+        //System.out.print("Tempo em minutos para imprimir as 180 com "+n+" impressoras:"+calc);
+        
+        //3
+        //Scanner entrada = new Scanner(System.in);
+        //double calc,c_n,d,litros,km;
+        
+        //litros = 18;
+        //km = 240;
+        //calc = litros/km;
+        
+        
+        //System.out.print("Digite a distância em km:");
+        //d = entrada.nextDouble();
+        
+        //c_n = calc*d;
+        //System.out.print("Litros de combustível necessários para "+d+" km:"+c_n);
+        
+        //4
+        //Scanner entrada = new Scanner(System.in);
+        //double m,h,p;
+        
+        ///System.out.print("Digite a quantidade de máquinas:");
+        //m = entrada.nextDouble();
+        
+        //System.out.print("Digite a quantidade de tempo(horas):");
+        //h = entrada.nextDouble();
+        
+        //p = (m*h) *30;
+        //System.out.print("Quantidade de peças produzidas:"+p);
+        
+        //5
+        //Scanner entrada = new Scanner(System.in);   
+        //double c,u,valorR;
+        
+        //System.out.print("Digite a cotação do Dólar:");
+        //c = entrada.nextDouble();
+        
+        //System.out.print("Digite a quantidade de dólares:");
+        //u = entrada.nextDouble();
+        
+        //valorR = c*u;
+        
+        //System.out.print("Para comprar "+u+" dólares, você precisará de:"+valorR);
+        
+        //6
+        //Scanner entrada = new Scanner(System.in);  
+        //double a,b,x,calc;
+        
+        //System.out.print("Digite um valor para a:");
+        //a = entrada.nextDouble();
+        
+        //System.out.print("Digite um valor para b:");
+        //b = entrada.nextDouble();
+        
+        //x = -b/a;
+        //calc = (a*x)+ b+0;
+        
+        //System.out.print("Valor de a:"+a);
+        //System.out.print("Valor de b:"+b);
+        //System.out.print("Valor de x:"+x);
+        
+        //7
+        //Scanner entrada = new Scanner(System.in);  
+        //double m,b,x0,y0;
+        
+        //System.out.print("Digite um valor para m:");
+        ///m = entrada.nextDouble();
+        
+        //System.out.print("Digite um valor para b:");
+        //b = entrada.nextDouble();
+        
+        //System.out.print("Digite um valor para x0:");
+        //x0 = entrada.nextDouble();
+        
+        //y0 = m*x0+b;
+        
+        //System.out.print("Valor de y0:"+y0);
+        
+        //8
+        //Scanner entrada = new Scanner(System.in);  
+        //double x1,x2,y1,y2,m,b;
+        
+        //System.out.print("Digite um valor para x1:");
+        //x1 = entrada.nextDouble();
+        
+        //System.out.print("Digite um valor para x2:");
+        //x2 = entrada.nextDouble();
+        
+        //System.out.print("Digite um valor para y1:");
+        //y1 = entrada.nextDouble();
+        
+        //System.out.print("Digite um valor para y2:");
+        //y2 = entrada.nextDouble();
+        
+        //m = (y2-y1)/(x2-x1);
+        //b = y1-m*x1;
+        
+        //System.out.print("Equação: y = "+m+ "x "+b);
+        
+        //9
+        //Scanner entrada = new Scanner(System.in);  
+        //double m1,b1,m2,b2,x,y;
+        
+        //System.out.print("Digite um valor para m1:");
+        //m1 = entrada.nextDouble();
+        
+        //System.out.print("Digite um valor para b1:");
+        //b1 = entrada.nextDouble();
+        
+        //System.out.print("Digite um valor para m2:");
+        //m2 = entrada.nextDouble();
+        
+        //System.out.print("Digite um valor para b2:");
+        //b2 = entrada.nextDouble();
+        
+        //x = (b2-b1)/(m1-m2);
+        //y = m1*x+b1;
+        
+        //System.out.print("Valor de (x,y): ("+x+","+y+")");
+        
+        //10
+        //Scanner entrada = new Scanner(System.in);
+        //double a,b,c,x1,x2,delta;
+        
+        //System.out.print("Valor de a:");
+        //a = entrada.nextDouble();
+        
+        //System.out.print("Valor de b:");
+        //b = entrada.nextDouble();
+        
+        //System.out.print("Valor de c:");
+        //c = entrada.nextDouble();
+        
+        //delta = (Math.pow(b,2)) - (4*a*c);
+        //x1 = (-b + Math.sqrt(delta)/(2*a));
+        //x2 = (-b - Math.sqrt(delta)/(2*a));
+        
+        //System.out.print("Valor de x1:"+x1);
+        //System.out.print("Valor de x2:"+x2);
+        
+        //11
+        //Scanner entrada = new Scanner(System.in);
+        //double a,delta,d;
+        
+        //System.out.print("Valor de a:");
+        //a = entrada.nextDouble();
+        
+        //System.out.print("Valor de delta:");
+        //delta = entrada.nextDouble();
+        
+        //d = Math.abs((Math.sqrt(delta))/a);
+        
+        //System.out.print("Valor de d:"+d);
+        
+        //12
+        //Scanner entrada = new Scanner(System.in);
+        //double a,b,c,x0,f,f1;
+        
+        //System.out.print("Valor de a:");
+        //a = entrada.nextDouble();
+        
+        //System.out.print("Valor de b:");
+        //b = entrada.nextDouble();
+        
+        //System.out.print("Valor de c:");
+        //c = entrada.nextDouble();
+        
+        //System.out.print("Valor de x0:");
+        //x0 = entrada.nextDouble();
+        
+        //f = a*(Math.pow(x0,2))+b*x0+c;
+        //f1 = 2*a*x0+b;
+        
+        //System.out.print("Valor de f:"+f);
+        //System.out.print("Valor de f1:"+f1);
+        
+        //13
+        //Scanner entrada = new Scanner(System.in);
+        //double m,b,x1,x2,y1,y2,A;
+        
+        //System.out.print("Valor de m:");
+        //m = entrada.nextDouble();
+        
+        //System.out.print("Valor de b:");
+        //b = entrada.nextDouble();
+        
+        //System.out.print("Valor de x1:");
+        //x1 = entrada.nextDouble();
+        
+        //System.out.print("Valor de x2:");
+        //x2 = entrada.nextDouble();   
+        
+        //y1 = m*x1+b;
+        //y2 = m*x2+b;
+        //A = ((y1+y2)/2)*(x2-x1);
+        
+        //System.out.print("Valor de A:"+A);       
+    }   
+}
