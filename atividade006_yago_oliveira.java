08222058dceaba9e55b8d4be92dbac9f5f38290e
Yago Oliveira <yagooliveira8009@gmail.com>
2025-10-13 20:48:56 -0300

    Update atividade006_yago_oliveira.java

--- atividade006_yago_oliveira.java
+++ atividade006_yago_oliveira.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package atividade06_gabriel_avila;
+package atividade006_yagooliveiraJava;
 import java.util.Scanner;
 import java.lang.Math; 
 import java.util.Random;
@@ -12,7 +12,7 @@ import java.util.Scanner;
  *
  * @author Aluno
  */
-public class Atividade06_gabriel_avila {
+public class Atividade006_yagooliveiraJava{
 
     /**
      * @param args the command line arguments
@@ -161,3 +161,4 @@ public class Atividade06_gabriel_avila {
             
           }
     
+
